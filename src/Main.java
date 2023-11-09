95ec6260a97695a0363f6d24740479f707d550f5
g21turbo <dg21stew@gmail.com>
2023-11-08 21:53:11 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int currentYear = 2023;
+
+        System.out.println(getInputFromConsole(currentYear));
+
+        System.out.println(getInputFromScanner(currentYear));
+
+    }
+
+    public static String getInputFromConsole(int currentYear) {
+
+        String name = System.console().readLine("Hi, what's your name?");
+        System.out.println("Hi " + name + ", thanks for taking the course.");
+
+        String dateOfBirth = System.console().readLine("What year were you born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+
+        return "";
+    }
+
+}
